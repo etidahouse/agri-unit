d9911c108af94d424eaac80147516d7244b1937e
etidahouse <e-begon@hotmail.fr>
2025-06-11 14:07:52 +0200

    wip: system is changing constantly

--- deploy/init.sql
+++ deploy/init.sql
@@ -19,3 +19,21 @@ CREATE TABLE IF NOT EXISTS agricultural_unit_surveys (
     year INT NOT NULL,
     data JSONB NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS weather (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    archived_at TIMESTAMPTZ NULL,
+
+    agricultural_unit_id UUID NOT NULL,
+
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    temperature DOUBLE PRECISION NOT NULL,
+    humidity INT NOT NULL,
+    wind_speed DOUBLE PRECISION NOT NULL,
+    clouds INT NOT NULL,
+    weather_main TEXT NOT NULL,
+    weather_desc TEXT NOT NULL
+);
